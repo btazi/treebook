f7e50a95fd8b4c163c77062c5ba1473d168217c4
Badr Tazi <tasibadr@gmail.com>
2013-07-06 18:08:49 +0100

    updated profile page and added scoped status update to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -4,19 +4,22 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
 	test "that /login route opens the login page" do
 		get '/login'
 		assert_response :success 
-	
 	end
 
 	test "that /logout route opens the logout page" do
 		get '/logout'
 		assert_response :redirect
 		assert_redirected_to '/' 
-	
 	end
 
 	test "that /register route opens the register page" do
 		get '/register'
 		assert_response :success 
-	
 	end
+
+	test "that a profile page works" do
+		get '/jason123'
+		assert_response :success
+	end
+
 end
