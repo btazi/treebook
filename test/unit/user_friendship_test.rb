9fb89ecca6081e603c00b77e07cc285b65850571
Badr Tazi <tasibadr@gmail.com>
2013-07-07 18:06:59 +0100

    added shoulda to test group and worked on user & users friendships associations & tests

--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserFriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+should belong_to(:user)
+should belong_to(:friend)
+
+test "creating a friendship works without raising an exception" do
+	assert_nothing_raised do
+	UserFriendship.create user: users(:jason), friend: users(:bob)
+	end	
+end
+
+test "that creating a friendship based on user id and friend id works" do
+		UserFriendship.create user_id: users(:jason).id, friend_id: users(:bob).id
+		assert users(:jason).friends.include?(users(:bob))
+end
+
 end
